e3120d9f5fe909977f38affd73c115d94ec0ba1a
jamal-almaari <j.almaari707@gmail.com>
2024-06-03 18:00:04 +0300

    a first commit

---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+@section('title', $viewData['title'])
+@section('subtitle', $viewData['subtitle'])
+@section('content')
+
+    @if (session('worong_order'))
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>{{ session('worong_order') }}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+    <div class="card">
+        <div class="card-header">
+            Products in Cart
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered table-striped text-center">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($viewData['products'] as $product)
+                        <tr>
+                            <td>{{ $product->getId() }}</td>
+                            <td>{{ $product->getName() }}</td>
+                            <td>${{ $product->getPrice() }}</td>
+                            <td>{{ session('products')[$product->getId()] }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <div class="row">
+                <div class="text-end">
+                    <a class="btn btn-outline-secondary mb-2"><b>Total to pay:</b> ${{ $viewData['total'] }}</a>
+                    @if (count($viewData['products']) > 0)
+                        <a href="{{ route('cart.purchase') }}" class="btn bg-primary text-white mb-2">Purchase</a>
+                        <a href="{{ route('cart.delete') }}">
+                            <button class="btn btn-danger mb-2">
+                                Remove all products from Cart
+                            </button>
+                        </a>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
